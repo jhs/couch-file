35a99073935f7e64f52f762c1da3c42756cf6b1c
Jason Smith (iMac) <jason.h.smith@gmail.com>
2016-07-25 21:54:45 +0700

    XXX Mon Jul 25 21:54:45 ICT 2016

--- couch-btree.js
+++ couch-btree.js
@@ -14,7 +14,7 @@ exports.open = open
 
 //var fs = require('fs')
 var debug = require('debug')('couch-file:couch-btree')
-//var erlang = require('erlang')
+var erlang = require('erlang')
 //var crypto = require('crypto')
 
 //var couch_compress = require('./couch-compress.js')
@@ -107,6 +107,112 @@ class Btree {
       } else
         return callback(new Error(`Unknown stream result: ${JSON.stringify(result)}`))
     }
+  }
+
+  add(insert_keyvalues, callback) {
+    debug('add')
+    this.add_remove(insert_keyvalues, [], callback)
+  }
+
+  add_remove(insert_keyvalues, remove_keys, callback) {
+    debug('add_remove')
+    this.query_modify([], insert_keyvalues, remove_keys, (er, _, bt2) => {
+      debug('add_remove complete')
+      callback(er, bt2)
+    })
+  }
+
+  query_modify(lookup_keys, insert_values, remove_keys, callback) {
+    var self = this
+    debug('query_modify')
+
+    var insert_actions = insert_values.map(keyvalue => {
+      var kv = self.extract_kv(keyvalue)
+      return {op:'insert', key:kv.key, value:kv.value}
+    })
+    var remove_actions = remove_keys.map(key => ({op:'remove', key:key, value:null}))
+    var fetch_actions = lookup_keys.map(key => ({op:'fetch', key:key, value:null}))
+
+    // For ordering different operations with the same key. fetch < remove < insert
+    var op_order = {fetch:1, remove:2, insert:3}
+    function sortfun(A, B) {
+      if (A.key == B.key) {
+        // A and B are equal, sort by op.
+        return op_order[A.op] - op_order[B.op]
+      } else
+        return self.less(A.key, B.key)
+    }
+
+    var actions = [].concat(insert_actions, remove_actions, fetch_actions)
+    actions.sort(sortfun)
+    debug('query_modify actions: %j', actions)
+
+    self.modify_node(self.root, actions, [], (er, key_pointers, query_results) => {
+      if (er)
+        return callback(er)
+
+      debug('query_modify: modify_node complete: %s key pointers', key_pointers.length)
+      debug('query_results = %j', query_results)
+      self.complete_root(key_pointers, (er, new_root) => {
+        if (er)
+          return callback(er)
+
+        var result = new Btree(self)
+        result.root = new_root
+        debug('query_modify: complete_root complete')
+        callback(null, query_results, result)
+      })
+    })
+  }
+
+  modify_node(root_pointer_info, actions, query_output, callback) {
+    var self = this
+    debug('modify_node: %j', root_pointer_info)
+
+    if (root_pointer_info === null) {
+      got_node(null, 'kv_node', [])
+    } else {
+      var pointer = erlang.element(1, root_pointer_info)
+      self.get_node(pointer, got_node)
+    }
+
+    function got_node(er, node_type, node_list) {
+      if (er)
+        return callback(er)
+
+      var node_tuple = erlang.list_to_tuple(node_list)
+      switch (node_type) {
+        case 'kp_node': self.modify_kpnode(node_tuple, 1, actions, [], query_output, modify_done); break
+        case 'kv_node': self.modify_kvnode(node_tuple, 1, actions, [], query_output, modify_done); break
+        default:
+          return callback(new Error(`Unknown node_type: ${node_type}`))
+      }
+
+      function modify_done(er, new_node_list, query_output2) {
+        if (er)
+          return callback(er)
+
+        if (new_node_list.length == 0) {
+          // No nodes remain.
+          return callback(null, [], query_output2)
+        } else if (is_same_node_list(node_list, new_node_list)) {
+          // Nothing changed.
+          var last_key = erlang.element(erlang.tuple_size(node_tuple), node_tuple).key
+          return callback(null, [{key:last_key, pointer:root_pointer_info}], query_output2)
+        } else
+          self.write_node(node_type, new_node_list, (er, result_list) => {
+            if (er)
+              return callback(er)
+            callback(null, result_list, query_output2)
+          })
+      }
+    }
+  }
+
+  modify_kvnode(node_tuple, lower_bound, actions, result_node, query_output, callback) {
+    debug('modify_kvnode')
+    //{ok, lists:reverse(ResultNode, bounded_tuple_to_list(NodeTuple, LowerBound, tuple_size(NodeTuple), [])), QueryOutput};
+    // XXX HERE
   }
 
   make_key_in_end_range_function(dir, options) {
